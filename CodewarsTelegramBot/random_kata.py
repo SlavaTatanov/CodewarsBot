2042f4ba5f46b1480a94db4ce4c90245af32455b
SlavaTatanov <slavatatanov@gmail.com>
2023-04-04 21:40:49 +0500

    TODO List

--- CodewarsTelegramBot/random_kata.py
+++ CodewarsTelegramBot/random_kata.py
@@ -1,10 +1,20 @@
 from CodewarsTelegramBot.conf import CONSTANCE
+from CodewarsTelegramBot.database.models import Langs
+
+
+def random_kata(langs_obj: list[Langs] | Langs) -> dict[str, int]:
+    pass  # TODO Сергей. Функция маршрутизатор, Поэтапно вызывает все функции и
+    # возвращает результат функции create_random_kata.
+
+
+def create_lang_dict(langs_obj: list[Langs] | Langs) -> dict[str, list]:
+    pass  # TODO Сергей
 
 
 def get_lang_levels(langs: dict) -> dict:
     """
     Принимает словарь язык: мин, мах уровни.
-    Возвращает словарь язык: [5, 6, 7]
+    Возвращает словарь {язык: [5, 6, 7]}
     """
     lang_levels = {}
     for key, val in langs.items():
@@ -19,13 +29,13 @@ def get_lang_list(langs: dict) -> list:
     те у которых приоритет выше, попадут в список больше раз
     и соответственно с большей вероятностью выпадут пользователю.\n
     "Normal", "High", "Very High" - варианты приоритетов
-    :param langs: Словарь вида {Язык: приоритет, ...}
+    :param langs: Словарь вида {Язык: {приоритет, min, max}, ...}
     :return: Список вида [Язык1, Язык1, Язык2]
     """
     lang_list = []
     for k, v in langs.items():
         iterations = 1
-        match v:
+        match v["priority"]:
             case CONSTANCE.NORMAL:
                 pass
             case CONSTANCE.HIGH:
@@ -35,3 +45,7 @@ def get_lang_list(langs: dict) -> list:
         for i in range(iterations):
             lang_list.append(k)
     return lang_list
+
+
+def create_random_kata(langs: list, langs_levels: dict[str, list]) -> dict[str, int]:
+    pass  # TODO Сергей. Вернуть словарь вида {"lang": "Python", "level": 6}. См. тесты
--- test/test_random_kata.py
+++ test/test_random_kata.py
@@ -1,15 +1,35 @@
 from unittest import TestCase
-from CodewarsTelegramBot.random_kata import get_lang_list, get_lang_levels
+from CodewarsTelegramBot.random_kata import get_lang_list, get_lang_levels, create_lang_dict, create_random_kata, \
+    random_kata
+from CodewarsTelegramBot.database.models import Langs
 
 
 class TestRandomKata(TestCase):
     def test_get_lang_list(self):
-        test_item_1 = {"Python": "High", "JavaScript": "Normal", "Haskell": "Very High"}
+        test_item_1 = {"Python": {"min": 8, "max": 6, "priority": "High"},
+                       "JavaScript": {"min": 7, "max": 4, "priority": "Normal"},
+                       "Haskell": {"min": 7, "max": 4, "priority": "Very High"}}
         self.assertEqual(get_lang_list(test_item_1), ["Python", "Python", "JavaScript",
                                                       "Haskell", "Haskell", "Haskell"])
 
     def test_get_lang_levels(self):
-        test_item = {"Haskell": {"min": 8, "max": 6}, "Python": {"min": 7, "max": 4}}
+        test_item = {"Haskell": {"min": 8, "max": 6, "priority": "High"},
+                     "Python": {"min": 7, "max": 4, "priority": "Normal"}}
         self.assertEqual(get_lang_levels(test_item), {"Haskell": [6, 7, 8], "Python": [4, 5, 6, 7]})
 
+    def test_create_lang_dict(self):
+        test_item1 = Langs(lang="Python", lang_min=6, lang_max=4, lang_priority="Normal")
+        test_item2 = [test_item1, Langs(lang="Haskell", lang_min=8, lang_max=6, lang_priority="High")]
+        self.assertEqual(create_lang_dict(test_item1), {"Python": {"min": 6, "max": 4, "priority": "Normal"}})
+        self.assertEqual(create_lang_dict(test_item2), {"Python": {"min": 6, "max": 4, "priority": "Normal"},
+                                                        "Haskell": {"min": 8, "max": 6, "priority": "High"}})
+
+    def test_create_random_kata(self):
+        test_langs = ["Python", "Python", "Haskell", "Haskell", "Haskell"]
+        test_levels = {"Haskell": [6, 7, 8], "Python": [4, 5, 6, 7]}
+        for res in [create_random_kata(test_langs, test_levels),
+                    random_kata(Langs(lang="Python", lang_min=6, lang_max=4, lang_priority="Normal"))]:
+            self.assertEqual(type(res), dict)
+            self.assertTrue("Haskell" == res["lang"] or "Python" == res["lang"])
+            self.assertTrue("lang" in res.keys() and "level" in res.keys())
 
